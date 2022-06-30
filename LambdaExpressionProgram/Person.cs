684ad855a45f2bcf4cd90e18de461b6577fd9f31
Vishwas <baghel.vishwas0@gmail.com>
2022-06-30 20:54:25 +0530

    Managing Person's Data In A List

---
+++ LambdaExpressionProgram/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressionProgram
+{
+    public class Person
+    {
+        public string SSN;
+        public string Name;
+        public string Address;
+        public int Age;
+
+        public Person(string ssn, string name, string adress, int age)
+        {
+            SSN = ssn;
+            Name = name;
+            Address = adress;
+            Age = age;
+        }
+    }
+}
---
+++ LambdaExpressionProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace LambdaExpressionProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Lambda Expression Program\n========================================");
+
+            List<Person> personlist = new List<Person>();
+            personlist.Add(new Person("123654", "Vishwas", "Jabalpur", 28));
+            personlist.Add(new Person("123656", "Vivek", "Mumbai", 15));
+            personlist.Add(new Person("123657", "Siddhant", "Pune", 62));
+            personlist.Add(new Person("123659", "Shobhit", "Indore", 20));
+            personlist.Add(new Person("123658", "Avinash", "Bhopal", 60));
+            personlist.Add(new Person("123678", "Sagar", "Patna", 78));
+            personlist.Add(new Person("129668", "Priyanshu", "Noida", 58));
+            personlist.Add(new Person("126662", "Ankit", "Bhoma", 48));
+
+        }
+    }
+}
