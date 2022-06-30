989fe3504e7d5f13ad252e0691dd70110186b27d
Vishwas <baghel.vishwas0@gmail.com>
2022-06-30 22:59:55 +0530

    [Vishwas]add: Retrieving Data For 13 to 18 Year Old

--- LambdaExpressionProgram/Program.cs
+++ LambdaExpressionProgram/Program.cs
@@ -8,14 +8,14 @@ namespace LambdaExpressionProgram
             Console.WriteLine("Welcome to the Lambda Expression Program\n========================================");
 
             List<Person> personlist = new List<Person>();
-            personlist.Add(new Person("123654", "Vishwas", "Jabalpur", 28));
-            personlist.Add(new Person("123656", "Vivek", "Mumbai", 15));
-            personlist.Add(new Person("123657", "Siddhant", "Pune", 62));
-            personlist.Add(new Person("123659", "Shobhit", "Indore", 20));
+            personlist.Add(new Person("123654", "Vishwas", "Jabalpur", 18));
+            personlist.Add(new Person("123656", "Vivek", "Mumbai", 65));
+            personlist.Add(new Person("123657", "Siddhant", "Pune", 22));
+            personlist.Add(new Person("123659", "Shobhit", "Indore", 16));
             personlist.Add(new Person("123658", "Avinash", "Bhopal", 60));
-            personlist.Add(new Person("123678", "Sagar", "Patna", 78));
+            personlist.Add(new Person("123678", "Sagar", "Patna", 68));
             personlist.Add(new Person("129668", "Priyanshu", "Noida", 58));
-            personlist.Add(new Person("126662", "Ankit", "Bhoma", 48));
+            personlist.Add(new Person("126662", "Ankit", "Bhoma", 14));
             
             
             Console.WriteLine("Retrieving Top 2 aged person from the list less than 60 years");
@@ -24,6 +24,11 @@ namespace LambdaExpressionProgram
                 Console.WriteLine("Name : " + person.Name + "\t\tAge : " + person.Age);
             }
 
+            Console.WriteLine("\n Checking whether any one of them teenager or not");
+            foreach (Person person in personlist.FindAll(e => (e.Age >= 13 && e.Age <= 18)))
+            {
+                Console.WriteLine("Name : " + person.Name + "\t\tAge : " + person.Age);
+            }
         }
     }
 }
