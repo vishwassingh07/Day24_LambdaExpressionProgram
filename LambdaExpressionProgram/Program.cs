b11f6c74dde633ec8f408082a02f540ebcf6f6bf
Vishwas <baghel.vishwas0@gmail.com>
2022-06-30 21:01:53 +0530

    [Vishwas]add: Retrieving Data For Top Two Person Less Than 60 Year Old

--- LambdaExpressionProgram/Program.cs
+++ LambdaExpressionProgram/Program.cs
@@ -16,6 +16,13 @@ namespace LambdaExpressionProgram
             personlist.Add(new Person("123678", "Sagar", "Patna", 78));
             personlist.Add(new Person("129668", "Priyanshu", "Noida", 58));
             personlist.Add(new Person("126662", "Ankit", "Bhoma", 48));
+            
+            
+            Console.WriteLine("Retrieving Top 2 aged person from the list less than 60 years");
+            foreach (Person person in personlist.FindAll(e => (e.Age < 60)).Take(2).ToList())
+            {
+                Console.WriteLine("Name : " + person.Name + "\t\tAge : " + person.Age);
+            }
 
         }
     }
